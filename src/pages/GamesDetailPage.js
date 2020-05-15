005422b60e954f166095d48d1c9625fc3bb3c8fb
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2020-05-15 16:36:04 -0300

    Iniciando criação de card com informações do jogo

--- src/pages/GamesDetailPage.js
+++ src/pages/GamesDetailPage.js
@@ -1,12 +1,50 @@
 import React from 'react';
-import { View, ScrollView } from 'react-native';
+import { View, ScrollView, StyleSheet, Image, Text } from 'react-native';
 
 export default class PeopleDetailPage extends React.Component {
+
+    getPlatformsNames(game) {
+        const listSize = game.platforms.length;
+        let platforms = '';
+        
+        game.platforms.forEach((platform, index) => {
+            platforms += platform.platform.name + (index == (listSize - 1) ? '.' : ', '); 
+
+        });
+
+
+        return platforms;
+    }
+
+
     render(){
+        const { game } = this.props.navigation.state.params;
+
+        const platforms = this.getPlatformsNames(game);
+        
         return (
-            <ScrollView>
+            <ScrollView 
+                style={styles.container}>
+                <Image 
+                    source={{ uri: game.background_image }}
+                    style={styles.image}/>
+                <View>
+                    <Text>
+                        Plataformas
+                    </Text>
+                    <Text>{platforms}</Text>
+                </View>
                 
             </ScrollView>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 15
+    }, 
+    image: {
+        aspectRatio: 1
+    }
+});
